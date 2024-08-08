980f6dba8637147367449f967fc23e7410b8783d
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:50 -0700

    style navbar

--- ufoc/src/app/components/navbar/index.tsx
+++ ufoc/src/app/components/navbar/index.tsx
@@ -1,11 +1,16 @@
+import Link from "next/link";
 import styles from "./navbar.module.scss";
+
+import Banner from "../../../../public/banner.svg";
 
 const Navbar = () => {
 	return (
 		<div className={styles.navbar}>
-			<div></div>
+			<div className={styles.banner}>
+				<Banner />
+			</div>
 
-			<div className={styles.item}>
+			<div className={styles.usermenu}>
 				<span className={styles.username}>User</span>
 			</div>
 		</div>
