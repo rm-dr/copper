3bf3b02f380cc23b19ee976730d31a5b15356d33
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:52 -0700

    Added basic slider component

---
+++ ufoc/src/app/datasets/_slider/slider.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import styles from "./slider.module.scss";
+import clsx from "clsx";
+
+export function Slider(params: {
+	icon: ReactNode;
+	icon_text: string;
+	text: string;
+	right: ReactNode;
+
+	is_selected: boolean;
+	is_clickable: boolean;
+
+	onClick?: () => void;
+}) {
+	return (
+		<div
+			onMouseDown={(e) => {
+				if (e.button == 0 && params.is_clickable) {
+					if (params.onClick !== undefined) {
+						params.onClick();
+					}
+				}
+			}}
+			className={clsx(
+				styles.slider,
+				params.is_clickable && styles.clickable,
+				params.is_selected && styles.selected,
+			)}
+		>
+			<div className={styles.slider_left}>
+				<div className={styles.slider_left_icon}>{params.icon}</div>
+				<div className={styles.slider_left_text}>{params.icon_text}</div>
+			</div>
+			<div className={styles.slider_text}>{params.text}</div>
+			<div className={styles.slider_right}>{params.right}</div>
+		</div>
+	);
+}
