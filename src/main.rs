ff39cfd02f80fcf5c4b1c4b34a707c5d8b34efbc
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:40 -0700

    Final cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,18 @@
-use std::{
-	collections::HashMap,
-	fs::File,
-	io::{Read, Seek},
-};
+use std::{fs::File, io::Read};
 
 use anyhow::Result;
 
+mod ingest;
 mod model;
 mod pipeline;
 mod storage;
 
-use model::ItemReader;
 use storage::StorageBackend;
 
 use crate::{
-	model::{AudioItemType, ItemType},
-	pipeline::{
-		components::pipeline::Pipeline,
-		data::{AudioFormat, BinaryFormat, PipelineData},
-	},
+	ingest::{file::FileInjest, Ingest},
+	pipeline::syntax::{prepareresult::PipelinePrepareResult, spec::PipelineSpec},
 };
-
-struct FileReader(File);
-
-impl Read for FileReader {
-	fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-		self.0.read(buf)
-	}
-}
-
-impl Seek for FileReader {
-	fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
-		self.0.seek(pos)
-	}
-}
-
-impl ItemReader<'_> for FileReader {}
 
 fn main() -> Result<()> {
 	let mut x = storage::MemStorageBackend::new();
@@ -47,50 +24,19 @@ fn main() -> Result<()> {
 	let mut f = File::open("pipeline.toml").unwrap();
 	let mut s: String = Default::default();
 	f.read_to_string(&mut s)?;
-	let mut p: Pipeline = toml::from_str(&s)?;
-	println!("{:?}", p.check());
+	let mut p: PipelineSpec = toml::from_str(&s)?;
+	let p = p.prepare();
+	let p = match p {
+		PipelinePrepareResult::Ok(x) => x,
+		_ => panic!(),
+	};
 
 	// Run pipeline
-	let f = File::open("data/freeze.flac").unwrap();
-	let mut f = FileReader(f);
-	x.add_item(d, ItemType::Audio(AudioItemType::Flac), &mut f)
-		.unwrap();
+	let f = FileInjest::new("data/freeze.flac".into());
+	println!("{:#?}\n\n", p.run(f.injest().unwrap())?);
 
-	f.rewind()?;
-
-	let mut data = Vec::new();
-	f.read_to_end(&mut data)?;
-
-	println!(
-		"{:#?}",
-		p.run(HashMap::from([(
-			"data".into(),
-			Some(PipelineData::Binary {
-				format: BinaryFormat::Audio(AudioFormat::Flac),
-				data
-			})
-		)]))?
-	);
-
-	let f = File::open("data/top.mp3").unwrap();
-	let mut f = FileReader(f);
-	x.add_item(d, ItemType::Audio(AudioItemType::Mp3), &mut f)
-		.unwrap();
-
-	f.rewind()?;
-	let mut data = Vec::new();
-	f.read_to_end(&mut data)?;
-
-	println!(
-		"{:#?}",
-		p.run(HashMap::from([(
-			"data".into(),
-			Some(PipelineData::Binary {
-				format: BinaryFormat::Audio(AudioFormat::Mp3),
-				data
-			})
-		)]))?
-	);
+	let f = FileInjest::new("data/top.mp3".into());
+	println!("{:#?}", p.run(f.injest().unwrap())?);
 
 	Ok(())
 }
