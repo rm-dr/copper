52912d8b400d909e295e3a931ac2d85b9073e768
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:44 -0700

    Added indices

--- crates/storage/src/sqlite/init_db.sql
+++ crates/storage/src/sqlite/init_db.sql
@@ -1,6 +1,9 @@
 -- Database metadata
 CREATE TABLE IF NOT EXISTS meta_meta (
-	var TEXT NOT NULL UNIQUE,
+	-- The name of the metadata variable
+	var TEXT PRIMARY KEY NOT NULL UNIQUE,
+
+	-- The value of the variable
 	val TEXT NOT NULL
 );
 
@@ -12,13 +15,15 @@ CREATE TABLE IF NOT EXISTS meta_classes (
 	pretty_name TEXT NOT NULL
 );
 
+CREATE INDEX IF NOT EXISTS idx_meta_class_name on meta_classes(pretty_name);
+
 
 -- Attribute metadata
 CREATE TABLE IF NOT EXISTS meta_attributes (
 	id INTEGER PRIMARY KEY NOT NULL,
 
 	-- The class this attribute belongs to
-	class_id INTEGER,
+	class_id INTEGER NOT NULL,
 
 	-- This attr's display name
 	pretty_name TEXT NOT NULL,
@@ -38,3 +43,48 @@ CREATE TABLE IF NOT EXISTS meta_attributes (
 	-- Attribute names must be unique within a class
 	UNIQUE (pretty_name, class_id)
 );
+
+CREATE INDEX IF NOT EXISTS idx_meta_attr_name on meta_attributes(pretty_name);
+CREATE INDEX IF NOT EXISTS idx_meta_attr_class on meta_attributes(class_id);
+
+
+-- Enum metadata
+CREATE TABLE IF NOT EXISTS meta_enums (
+	id INTEGER PRIMARY KEY NOT NULL,
+
+	-- The item class this enum belongs to
+	class_id INTEGER NOT NULL,
+
+	-- This enum's display name
+	pretty_name TEXT NOT NULL,
+
+	-- Boolean (0 or 1). Can we select multiple variants of this enum at once?
+	is_multi INTEGER NOT NULL,
+
+	FOREIGN KEY (class_id) REFERENCES meta_classes(id)
+	UNIQUE (pretty_name, class_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_meta_enum_name on meta_enums(pretty_name);
+CREATE INDEX IF NOT EXISTS idx_meta_enum_class on meta_enums(class_id);
+
+
+
+
+-- Enum variants
+CREATE TABLE IF NOT EXISTS meta_enum_variants (
+	id INTEGER PRIMARY KEY NOT NULL,
+
+	-- The enum this variant belongs to
+	enum_id INTEGER NOT NULL,
+
+	-- This variant's display name
+	pretty_name TEXT NOT NULL,
+
+	FOREIGN KEY (enum_id) REFERENCES meta_enums(id)
+	UNIQUE (pretty_name, enum_id)
+);
+
+CREATE INDEX IF NOT EXISTS idx_meta_enum_variant_name on meta_enum_variants(pretty_name);
+CREATE INDEX IF NOT EXISTS idx_meta_enum_variant_enum on meta_enum_variants(enum_id);
+
