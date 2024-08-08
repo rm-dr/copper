b0776aa2ab53798d625ac8a3077d32c69f75c002
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:56 -0700

    Added basic edit page

---
+++ ufoc/src/app/u/items/_edit/index.tsx
@@ -0,0 +1,58 @@
+import styles from "./edit.module.scss";
+import { Panel } from "@/app/components/panel";
+
+import { XIconArrowRight, XIconEdit } from "@/app/components/icons";
+import { ItemData, Selected } from "../page";
+import { attrTypes } from "../../datasets/_tree/attrs";
+import { Button } from "@mantine/core";
+
+export function EditPanel(params: { data: ItemData; select: Selected }) {
+	const selected = params.data.data[params.select.selected[0]];
+	console.log(selected);
+
+	return (
+		<>
+			<Panel
+				panel_id={styles.panel_edititem}
+				icon={<XIconEdit />}
+				title={"Edit items"}
+			>
+				<div className={styles.edit_container}>
+					{selected === undefined
+						? null
+						: Object.entries(selected.attrs)
+								.sort()
+								.map(([attr, val]: [string, any]) => {
+									const d = attrTypes.find((x) => {
+										return x.serialize_as === val.type;
+									});
+									if (d === undefined) {
+										return null;
+									}
+
+									return (
+										<div
+											key={`${selected.idx}-${attr}`}
+											className={styles.editrow}
+										>
+											<div className={styles.editrow_icon}>{d.icon}</div>
+											<div className={styles.editrow_name}>{attr}</div>
+											<div className={styles.editrow_value_old}>
+												{d.old_value !== undefined
+													? d.old_value({ attr: val })
+													: null}
+											</div>
+											<div className={styles.editrow_value_new}>
+												{d.new_value !== undefined
+													? d.new_value({ attr: val, onChange: console.log })
+													: null}
+											</div>
+										</div>
+									);
+								})}
+				</div>
+			</Panel>
+		</>
+	);
+}
+
