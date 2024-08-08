23b2bf3b75cdb7a0ddcf78266f3ce2739f013741
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-08-07 17:18:56 -0700

    Added edit panel draft

--- ufoc/src/app/u/items/_edit/index.tsx
+++ ufoc/src/app/u/items/_edit/index.tsx
@@ -1,10 +1,18 @@
 import styles from "./edit.module.scss";
 import { Panel } from "@/app/components/panel";
 
-import { XIconArrowRight, XIconEdit } from "@/app/components/icons";
+import {
+	XIconArrowRight,
+	XIconAttrBinary,
+	XIconEdit,
+	XIconTrash,
+	XIconUpload,
+} from "@/app/components/icons";
 import { ItemData, Selected } from "../page";
 import { attrTypes } from "@/app/_util/attrs";
-import { Button } from "@mantine/core";
+import { ActionIcon, Button, Text } from "@mantine/core";
+import Image from "next/image";
+import { ppBytes } from "@/app/_util/ppbytes";
 
 export function EditPanel(params: { data: ItemData; select: Selected }) {
 	const selected = params.data.data[params.select.selected[0]];
@@ -17,7 +25,7 @@ export function EditPanel(params: { data: ItemData; select: Selected }) {
 				icon={<XIconEdit />}
 				title={"Edit items"}
 			>
-				<div className={styles.edit_container}>
+				<div className={styles.edit_container_rows}>
 					{selected === undefined
 						? null
 						: Object.entries(selected.attrs)
@@ -54,8 +62,55 @@ export function EditPanel(params: { data: ItemData; select: Selected }) {
 									);
 								})}
 				</div>
+				<div className={styles.edit_container_panel}>
+					<div className={styles.paneltitle}>
+						<div className={styles.paneltitle_icon}>
+							<XIconAttrBinary />
+						</div>
+						<div className={styles.paneltitle_name}>cover_art</div>
+					</div>
+					<div className={styles.panelbody}>
+						<div className={styles.panelimage}>
+							<Image src="/cover.jpg" fill alt="Picture of the author" />
+						</div>
+					</div>
+					<div className={styles.panelbottom}>
+						<div>
+							<Text>{ppBytes(100088)}</Text>
+						</div>
+						<div style={{ flexGrow: 1 }}>
+							<Text ff="monospace">image/png</Text>
+						</div>
+						<div>
+							<ActionIcon variant="filled" color="red">
+								<XIconTrash style={{ width: "70%", height: "70%" }} />
+							</ActionIcon>
+						</div>
+						<div>
+							<ActionIcon variant="filled">
+								<XIconUpload style={{ width: "70%", height: "70%" }} />
+							</ActionIcon>
+						</div>
+					</div>
+				</div>
 			</Panel>
 		</>
 	);
 }
 
+function OldValue(params: { attr: any }) {
+	<Button
+		radius="0px"
+		variant="outline"
+		fullWidth
+		rightSection={<XIconArrowRight />}
+	>
+		View in panel
+	</Button>;
+}
+
+function NewValue(params: { attr: any }) {
+	<Button radius="0px" variant="outline" fullWidth rightSection={<XIconEdit />}>
+		Edit in panel
+	</Button>;
+}
