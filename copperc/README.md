5debbabe7fa4dd77f99ef6701644c4bf08f577d0
rm-dr <96270320+rm-dr@users.noreply.github.com>
2024-09-13 23:17:19 -0700

    Delete old webui

--- copperc/README.md
+++
@@ -1,6 +0,0 @@
-# Copper web interface
-
-## Setup
-- `npm` may be replaced with `bun`.
-- `npm install`
-- `npm run dev`
--- copperc/src/lib/api/index.ts
+++
@@ -1,6 +0,0 @@
-import createClient from "openapi-fetch";
-import { paths } from "./openapi";
-
-export const APIclient = createClient<paths>({
-	baseUrl: "/api/",
-});
